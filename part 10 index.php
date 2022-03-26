aa6926f0e9767caa0c68b817ad5cc07c9ee86ddf
Gloria Kezia <91767970+Gloriakezia@users.noreply.github.com>
2022-03-26 11:01:20 +0900

    add part 10

---
+++ part 10 index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+    
+<?php
+
+    // Comparisiom Operators
+    $x = 10;
+    $y = 10;
+
+    if ($x >= $y) {
+        echo "True!";
+    }
+    else {
+        echo "False"
+    }
+
+?>
+</body>
+</html>
