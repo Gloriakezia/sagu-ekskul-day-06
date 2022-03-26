c3f476b8d148ba0a797c54615eecd2bdb16fd855
Gloria Kezia <91767970+Gloriakezia@users.noreply.github.com>
2022-03-26 10:45:01 +0900

    add part 7

---
+++ part 7 index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+
+<?php
+
+    //String
+    $name = 'Coding is fun';
+    
+    //Integer
+    $name = 20;
+
+    //Float
+    $name = 20.5784;
+
+    //Boolean
+    true = 1
+    false = 0
+
+    //Array
+    $names = array ("Daniel","Dennis","Michael");
+    echo $names['1'];
+
+
+?>
+    
+</body>
+</html>
