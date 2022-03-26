8315e7e5ae84ab306ac85d8ab1333cf83f559f46
Gloria Kezia <91767970+Gloriakezia@users.noreply.github.com>
2022-03-26 10:37:49 +0900

    add part 5

---
+++ part 5 index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+    
+<?php
+    //thid gets something from the database
+    function() {
+        dksahoifasoifas
+    }
+ 
+
+?>
+
+
+</body>
+</html>
