f7a228353e47e49171cdf96b51eea0a3a462963d
Gloria Kezia <91767970+Gloriakezia@users.noreply.github.com>
2022-04-02 11:40:42 +0900

    push part 14

---
+++ part 14 index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+    
+<?php
+
+    $x = "2";
+    
+    switch ($x) {
+        case 1:
+            echo "The answer is 1";
+        break;
+        case 2:
+            echo "The answer is 2";
+        break;
+        case 3:
+            echo "The answer is 3";
+        break;
+        case 4:
+            echo "The answer is 4";
+        break;
+        default:
+    }
+
+?>
+</body>
+</html>
