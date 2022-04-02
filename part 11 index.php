e3f569a981f16e2588763708e48c8a5eb614edf0
Gloria Kezia <91767970+Gloriakezia@users.noreply.github.com>
2022-04-02 11:09:50 +0900

    push part 11

---
+++ part 11 index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+    
+<?php
+
+        //Increment/Decrement Operators
+        $x = 10;
+        echo $x--;
+        echo $x;
+
+?>
+</body>
+</html>
