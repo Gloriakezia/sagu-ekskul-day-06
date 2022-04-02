227041b001fa698158d4665a26388018fd7e86e3
Gloria Kezia <91767970+Gloriakezia@users.noreply.github.com>
2022-04-02 11:15:48 +0900

    push part 12

---
+++ part 12 index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+    
+<?php
+
+    //Logical Operators
+    $x = 10;
+    $y = 20;
+
+    if ($x == $y xor 1 == 1) {
+        echo "True";
+    }
+
+?>
+</body>
+</html>
