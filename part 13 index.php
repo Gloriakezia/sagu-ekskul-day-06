e903bcea3c64ddd3511deef5b1545ddf6e014796
Gloria Kezia <91767970+Gloriakezia@users.noreply.github.com>
2022-04-02 11:33:07 +0900

    push part 13

---
+++ part 13 index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+    
+<?php
+
+    $x = 1; 
+
+    if ($x == 1) {
+        echo "Daniel is very handsome!";
+     }
+    elseif ($x == 2 ) {
+         echo "Daniel is kinda handsome!";
+     }
+     elseif ($x == 3) {
+        echo "Daniel is kinda handsome!";
+     }
+     elseif ($x == 4) {
+        echo "Daniel is kinda handsome!";
+     }
+     else {
+         echo "Daniel is very ugly!";
+     }
+
+
+?>
+</body>
+</html>
