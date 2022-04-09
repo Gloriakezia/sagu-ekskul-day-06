ba266ccf4943df8ce12267454e10dc89982f6fb5
Gloria Kezia <91767970+Gloriakezia@users.noreply.github.com>
2022-04-09 11:17:56 +0900

    push part 20

---
+++ part 20 index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+    
+
+<?php
+    //Loop
+
+    //While loop
+    //Do while Loop
+    //For Loop
+    //Foreach Loop
+    $array = array("Daniel", "Jane", "Jacob", "John", "Mariane");
+
+    foreach ($array as $loopdata) {
+        echo "My name is ".$loopdata."<br>;
+    }
+?>
+</body>
+
+</html>
