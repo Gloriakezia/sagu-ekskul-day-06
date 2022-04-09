5c27b87f76b7315b175d6d1a75235dbf1ac3ae27
Gloria Kezia <91767970+Gloriakezia@users.noreply.github.com>
2022-04-09 11:23:18 +0900

    push pt 21

---
+++ part 21 index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+    
+
+<?php
+    
+    $array = array("Daniel", "Jane", "Jacob", "John", "Mariane");
+
+     echo $array[2];
+    
+?>
+</body>
+
+</html>
