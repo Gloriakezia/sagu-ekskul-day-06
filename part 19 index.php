0bec1709c33afdd63f41b164a2b45a350af9fb91
Gloria Kezia <91767970+Gloriakezia@users.noreply.github.com>
2022-04-09 11:05:00 +0900

    pust pt 18

---
+++ part 19 index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+    
+
+<?php
+    //Loop
+
+    //While loop
+    //Do while Loop
+    //For Loop
+    //Foreach Loop
+   
+    for ($x = 1; $x <= 10; $x++) {
+        echo "hi<br>";
+    }
+?>
+</body>
+
+</html>
