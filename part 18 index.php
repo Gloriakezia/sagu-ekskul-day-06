3c5b59e3b9b3479e0314d3f85b84074cc01af92e
Gloria Kezia <91767970+Gloriakezia@users.noreply.github.com>
2022-04-09 10:54:24 +0900

    push pt 18

---
+++ part 18 index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+    
+
+<?php
+    //Loop
+
+    //While loop
+    //Do while Loop
+    //For Loop
+    //Foreach Loop
+    $x = 1;
+    do {
+        echo "hi there<br>";
+        $x++;
+    }
+    while ($x <= 5) 
+?>
+</body>
+
+</html>
