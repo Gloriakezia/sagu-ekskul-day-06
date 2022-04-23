f0588bd74a62a0969d67812884c9edcc1b484f1d
Gloria Kezia <91767970+Gloriakezia@users.noreply.github.com>
2022-04-23 10:47:51 +0900

    push it

---
+++ part 24/part 24 index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+
+<?php
+ 
+$x = 5;
+
+function something() {
+    $y = 10;
+}
+
+echo $y;
+?>
+
+</body>
+
+</html>
+
+
+
