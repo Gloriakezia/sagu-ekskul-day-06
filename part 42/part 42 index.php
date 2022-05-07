7f66785f51ddd3f80d3b41407a4f2330e88bf107
Gloria Kezia <91767970+Gloriakezia@users.noreply.github.com>
2022-05-07 10:56:56 +0900

    push

--- part 42/part 42 index.php
+++ part 42/part 42 index.php
@@ -16,7 +16,7 @@
         <button type="submit" name="submit">Sign up</button>
         </form>
         <?php
-        $fullUrl = "https;//$_SERVER[HTTPS-HOST]$_SERVER[REQUEST_URI];
+        $fullUrl = "http://$_SERVER[HTTP-HOST]$_SERVER[REQUEST_URI]";
 
         if (strops($fullUrl, "signup=empty") == true){
             echo "<p class='error'You did not fill in all fields!</p>";
