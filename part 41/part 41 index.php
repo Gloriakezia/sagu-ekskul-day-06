b256b68acf6af175bac69bede5673f969c433c40
Gloria Kezia <91767970+Gloriakezia@users.noreply.github.com>
2022-05-07 09:56:03 +0900

    push

---
+++ part 41/part 41 index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+     <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Signup</h2>
+    <form action="includes/signup.inc.php" method="POST">
+    <input type="text" name="first" placeholder= "Firstname">
+    <input type="text" name="first" placeholder= "Firstname">
+    <input type="text" name="first" placeholder= "Firstname">
+    <input type="text" name="first" placeholder= "Firstname">
+    <input type="text" name="first" placeholder= "Firstname">
+    <button type="submit" name="submit">Sign up</button>
+    </form>
+</body>
+</html>
