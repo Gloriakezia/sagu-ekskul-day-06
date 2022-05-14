6e359714f6baa21dd2240ac2f1cbb3580875bb06
Gloria Kezia <91767970+Gloriakezia@users.noreply.github.com>
2022-05-14 10:19:33 +0900

    push

---
+++ part 50/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title></title>
+</head>
+<body>
+
+<?php
+    $data = array(
+        array(1,2,3),
+        "John",
+        "Jane"
+        );
+
+        echo $data[0] [1];
+
+?>
+</body>
+</html>
