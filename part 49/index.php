4077ff101874b7bd47d4404e7664e282ea2287d1
Gloria Kezia <91767970+Gloriakezia@users.noreply.github.com>
2022-05-14 10:13:34 +0900

    push

---
+++ part 49/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title></title>
+</head>
+<body>
+
+<?php
+
+$data["first"] = "Daniel";
+$data["last"] = "Nielsen";
+$data["age"]= 25;
+print_r($data);
+
+?>
+</body>
+</html>
