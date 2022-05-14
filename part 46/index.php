73997fdcc3ababa5dd5cf3c2d3a1a68a10ab887c
Gloria Kezia <91767970+Gloriakezia@users.noreply.github.com>
2022-05-14 09:47:41 +0900

    push

---
+++ part 46/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title></title>
+</head>
+<body>
+
+<?php
+   
+$data = array("first", "second");
+array_push($data, "Daniel", 15, 23);
+print_r($data);
+
+?>
+</body>
+</html>
