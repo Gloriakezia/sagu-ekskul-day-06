f9b876fa2e3a3fdc1272e0c734a73bc5261ae054
Gloria Kezia <91767970+Gloriakezia@users.noreply.github.com>
2022-05-14 09:33:51 +0900

    push

---
+++ Part 45/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title></title>
+</head>
+<body>
+
+<?php
+   
+$person = "Daniel";
+$people = array("Daniel", "Jane", "John");
+echo $people[0];
+
+?>
+</body>
+</html>
